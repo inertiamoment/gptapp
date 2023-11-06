d27e26c13e2a58efc4df80a69aa0abdc886c4039
jinsei.kuroki <kurojinwork13@gmail.com>
2023-11-06 20:25:59 +0900

    newver

--- src/main.js
+++ src/main.js
@@ -1,38 +1,18 @@
-<<<<<<< HEAD
 import Vue from 'vue';
 import { initializeApp } from 'firebase/app';
-import App from './App.vue';
 import 'firebase/functions';
 
 Vue.config.productionTip = false;
 
+require('dotenv').config();
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyC8f9oaTPzElvQ-j8knKshl-KopgUHl0xI',
-  authDomain: 'zundagpt-eecfd.firebaseapp.com',
-  projectId: 'zundagpt-eecfd',
-  storageBucket: 'zundagpt-eecfd.appspot.com',
-  messagingSenderId: '207907169112',
-  appId: '1:207907169112:web:414f6e012c8cd7431bfb2d',
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
 };
 
 initializeApp(firebaseConfig);
-=======
-// main.js
-import { initializeApp } from 'firebase/app';
-import Vue from 'vue';
-import App from './App.vue';
-import './app.css';
-
-const firebaseConfig = {
-};
-Vue.config.productionTip = false;
->>>>>>> origin/main
-
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
-<<<<<<< HEAD
-=======
-
-initializeApp(firebaseConfig);
->>>>>>> origin/main
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  // その他の設定
-  transpileDependencies: [
-    'vue-echarts', // トランスパイルしたい依存関係を追加
-    // 他の依存関係も必要に応じて追加
-  ],
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        path: require.resolve('path-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        process: require.resolve('process/browser'),
+      },
+    },
+  },
 };
